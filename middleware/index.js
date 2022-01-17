d5c2247a52f2438c4bd0e8acaab3c1eb34d69ee5
SamarMohamed1 <samar.mohamed.hassan@gmail.com>
2022-01-18 00:32:55 +0200

    task 5

---
+++ middleware/index.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");//3shn a2dar ageb altoken
+
+const logger = (req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+}
+
+const auth = (req, res, next) => {
+    try {
+        const user = jwt.verify(req.header("x-auth-token"), process.env.JWT_SECRET);
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "un-authorized" })
+    }
+}
+
+const catchAsyncErrors = (handler) => {
+    return async (req, res, next) => {
+        try {
+            await handler(req, res, next)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+module.exports = { logger, auth, catchAsyncErrors }
